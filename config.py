f10016dc5f0467e23fe7e2dc96e1dc45e2fd6946
alstacon <alstacon@gmail.com>
2023-03-30 21:27:05 +0300

    auth

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_PORT = os.environ.get('DB_PORT')
+
+
+SECRET_KEY_COOKIE = os.environ.get('SECRET_KEY')
+SECRET_KEY_MANAGER = os.environ.get('SECRET_KEY')
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
+
+from auth.auth import auth_backend
+from auth.db import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
 
 app = FastAPI(
     title='Trading App'
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix='/auth/jwt',
+    tags=['auth'],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix='/auth',
+    tags=['auth'],
 )
